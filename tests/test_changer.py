7f806cae9f36a9a60fd579cdef01432c68795725
Sam Jones <sam.jones494@googlemail.com>
2019-09-25 10:54:35 +0100

    handle sums requiring single coins

--- tests/test_changer.py
+++ tests/test_changer.py
@@ -1,4 +1,10 @@
-from changer.changer import func
+from changer import changer
 
-def test_func():
-    assert func(1) == 2
+
+def test_it_correctly_handles_sums_requiring_1_coin():
+    assert changer.change(1) == [1]
+    assert changer.change(2) == [2]
+    assert changer.change(5) == [5]
+    assert changer.change(10) == [10]
+
+
