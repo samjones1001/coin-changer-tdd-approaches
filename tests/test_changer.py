5a0411750f8a7c446694ab94c42c0be4a7b0acc0
Sam Jones <sam.jones494@googlemail.com>
2019-09-25 11:00:13 +0100

    handles sums requiring 3 coins

--- tests/test_changer.py
+++ tests/test_changer.py
@@ -13,3 +13,9 @@ def test_it_correctly_handles_sums_requiring_2_coins():
     assert changer.change(6) == [5, 1]
     assert changer.change(7) == [5, 2]
     assert changer.change(11) == [10, 1]
+
+
+def test_it_correctly_handles_sums_requiring_3_coins():
+    assert changer.change(9) == [5, 2, 2]
+    assert changer.change(13) == [10, 2, 1]
+    assert changer.change(23) == [20, 2, 1]
