e8acdb7ada4d4eaf7c89d3189a6c5fc28f313313
Sam Jones <sam.jones494@googlemail.com>
2019-09-25 10:58:20 +0100

    handles sums requiring 2 coins

--- changer/changer.py
+++ changer/changer.py
@@ -1,2 +1,7 @@
 def change(amount):
-    return [amount]
+    coins = []
+    for coin in (200, 100, 50, 20, 10, 5, 2, 1):
+        while amount >= coin:
+            coins.append(coin)
+            amount -= coin
+    return coins
--- tests/test_changer.py
+++ tests/test_changer.py
@@ -8,3 +8,8 @@ def test_it_correctly_handles_sums_requiring_1_coin():
     assert changer.change(10) == [10]
 
 
+def test_it_correctly_handles_sums_requiring_2_coins():
+    assert changer.change(3) == [2, 1]
+    assert changer.change(6) == [5, 1]
+    assert changer.change(7) == [5, 2]
+    assert changer.change(11) == [10, 1]
